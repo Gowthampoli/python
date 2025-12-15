8b0369d92295046e5eee89334970e9706ef3ecf8
Gowthampoli <poli.gowthamkumarreddy@gmail.com>
2025-12-15 10:57:39 +0530

    upload

---
+++ class2-/mutable.py
@@ -0,0 +1,20 @@
+enames=['Rahul','Sonia','Priya']
+enames[0]='Rahul Gandhi'
+print(enames)
+
+
+
+eid={101,101,101,102}
+print(eid)
+
+
+
+emp={'eid':101, 'ename': 'Rahul', 'easl':45000}
+emp[0]=102
+
+
+
+
+b=bytearray([10,20,30])
+b[0]=40
+print(b)
---
+++ class2-/typecasting.py
@@ -0,0 +1,13 @@
+price = 15
+a=int(price)
+x=bin(price)
+y=oct(price)
+z=hex(price)
+print(a)
+print(x)
+print(y)
+print(z)
+print(type(a))
+print(type(x))
+print(type(y))
+print(type(z))
