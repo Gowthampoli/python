8a67a74f8c8e2980b33f47b59f99e68940f6a447
Gowthampoli <poli.gowthamkumarreddy@gmail.com>
2025-12-19 10:48:57 +0530

    upload

---
+++ class5-tasks/task2.py
@@ -0,0 +1,13 @@
+languages = ["Python1", "Java", "C++", "JavaScript", "Ruby"]
+
+# Check if "Python" is present using 'in' operator
+if "Python" in languages:
+    print("Python is present in the list.")
+else:
+    print("Python is not present in the list.")
+
+# Check if "Python" is present using 'not in' operator
+if "Python" not in languages:
+    print("Python is not present in the list.")
+else:
+    print("Python is present in the list.")
